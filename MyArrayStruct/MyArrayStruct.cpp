a250db9dbcea05c35c943f4558a6210a9947f807
GrisWold Diablo <GrisWoldDiablo@users.noreply.github.com>
2017-12-20 19:10:00 -0500

    Small change to interface

--- MyArrayStruct/MyArrayStruct.cpp
+++ MyArrayStruct/MyArrayStruct.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-template <class T>
+template <typename T>
 struct MyArray {
+private:
 	T *arrayValue = NULL;
-	int theSize;
-	using reference = T&;
-
-	MyArray(initializer_list<T> iniList) {
-		int size = iniList.size();
+public:
+	size_t size;
+	MyArray(initializer_list<T> iniList = {}) {
+		size = iniList.size();
 		arrayValue = new T[size];
-		theSize = size;
-		for (int i = 0; i < theSize; i++) {
+		for (size_t i = 0; i < size; i++) {
 			arrayValue[i] = *(iniList.begin() + i);
 		}
 	}
 
-	const reference operator[](int i) {
+	T& operator[](int i) {
 		return arrayValue[i];
 	}
-
 	void addValue(T valueToAdd) {
-		theSize++;
-		T *tempArray = new T[theSize];
+		T *tempArray = new T[++size];
 
-		tempArray[theSize - 1] = valueToAdd;
+		tempArray[size - 1] = valueToAdd;
 
-		for (int i = 0; i < theSize - 1; i++) {
+		for (size_t i = 0; i < size - 1; i++) {
 			tempArray[i] = arrayValue[i];
 		}
 
@@ -37,72 +35,72 @@ struct MyArray {
 	}
 
 	void reSize(int newSize) {
-		int oldSize = theSize;
 		
-		T *tempArray = new T[newSize];
-
-		if (newSize > 0) {
-
-			if (newSize > oldSize) {
-				for (int i = 0; i < oldSize; i++) {
-
-					tempArray[i] = arrayValue[i];
-				}
-
-				for (int i = oldSize; i < newSize; i++) {
-
-					tempArray[i] = T{};
-				}
-			}
-			else {
-				for (int i = 0; i < newSize; i++) {
-
-					tempArray[i] = arrayValue[i];
-				}
-			}
-
+		
+		if (newSize <= 0) {
 			delete[] arrayValue;
-			arrayValue = tempArray;
+			arrayValue = NULL;
+			return;
+		}
 
+		size_t oldSize = size;
+		size = newSize;
+		T *tempArray = new T[newSize];
+	
+		if (newSize > oldSize) {
+			for (size_t i = 0; i < oldSize; i++) {
+
+				tempArray[i] = arrayValue[i];
+			}
+
+			for (size_t i = oldSize; i < newSize; i++) {
+
+				tempArray[i] = T{};
+			}
 		}
 		else {
-			delete[] arrayValue;
+			for (size_t i = 0; i < newSize; i++) {
+
+				tempArray[i] = arrayValue[i];
+			}
 		}
-		theSize = newSize;
+
+		delete[] arrayValue;
+		arrayValue = tempArray;
 
 	}
 };
 
 void main() {
 
+	vector<int> testmi = { 23,56,87,12,5,0,123,-345 };
 	int arraySize;
 	do {
-		cout << "What size: ";
-		//cin >> arraySize;
-		//MyArray<int> testThisArray(arraySize);
-		MyArray<string> testThisArray = {"hello","World" };
+		/*cout << "What size: ";
+		cin >> arraySize;
+		MyArray<string> testThisArray;
+		testThisArray.reSize(arraySize);*/
+		MyArray<string> testThisArray = { "hello","World" };
 		MyArray<int> thisIsIntArray = { 123,56,123,678,23,5,0 };
-		cout << "Value to add: ";
-		/*
-		int addV;
+		cout << "Value to add to array of String: ";
+		/*int addV;
 		cin >> addV;
-		cin.ignore();
-		*/
+		cin.ignore();*/
 		string addV;
 		getline(cin, addV);
 
 		testThisArray.addValue(addV);
 
-		for (int i = 0; i < testThisArray.theSize; i++) {
+		for (int i = 0; i < testThisArray.size; i++) {
 			cout << "Index: " << i << ", Value: " << testThisArray[i] << endl;
 		}
 		int index;
 		do {
 			
-			cout << "Index #: ";
+			cout << "Choose Index # to modify: ";
 			cin >> index;
 			cin.ignore();
-			if (index < testThisArray.theSize && index >= 0) {
+			if (index < testThisArray.size && index >= 0) {
 				cout << "New Value: ";
 				/*int newValue;
 				cin >> newValue;*/
@@ -114,24 +112,24 @@ void main() {
 		} while (index != -1);
 
 		int resizeIt;
-		cout << "Give New Size: ";
+		cout << "Give New Size to Array of String: ";
 		cin >> resizeIt;
 		testThisArray.reSize(resizeIt);
 
-		for (int i = 0; i < testThisArray.theSize; i++) {
+		for (int i = 0; i < testThisArray.size; i++) {
 			cout << "Index: " << i << ", Value: " << testThisArray[i] << endl;
 		}
 
 		// INT
-		for (int i = 0; i < thisIsIntArray.theSize; i++) {
+		for (int i = 0; i < thisIsIntArray.size; i++) {
 			cout << "Index: " << i << ", Value: " << thisIsIntArray[i] << endl;
 		}
 
-		cout << "Give New Size: ";
+		cout << "Give New Size to Array of INT: ";
 		cin >> resizeIt;
 		thisIsIntArray.reSize(resizeIt);
 
-		for (int i = 0; i < thisIsIntArray.theSize; i++) {
+		for (int i = 0; i < thisIsIntArray.size; i++) {
 			cout << "Index: " << i << ", Value: " << thisIsIntArray[i] << endl;
 		}
 
